b04d60947989f4f1ac4000edf97ed93a0c8a4ff9
User Petyan <petiti14771@mail.ru>
2024-09-14 01:39:57 +0300

    Done 10

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -33,7 +33,7 @@ public class FilmController {
         if (newFilm.getName() == null) {
             throw new ValidationException("Название фильма не может быть пустым");
         }
-        if (newFilm.getName().length() > 200) {
+        if (newFilm.getDescription().length() > 200) {
             throw new ValidationException("Название фильма не может содержать больше 200 символов");
         }
         if (LocalDate.of(1895, 12, 28).isAfter(newFilm.getReleaseDate())) {
@@ -77,7 +77,7 @@ public class FilmController {
         log.info("Updating film with id: {}", updatedFilm.getId());
 
         films.remove(updatedFilm.getId());
-        addFilm(updatedFilm);
+        films.put(updatedFilm.getId(), updatedFilm);
         return updatedFilm;
     }
 
